b132af77002b9cbda5f082e339a26dd57bc821d3
bernard-ng <ngandubernard@gmail.com>
2021-10-23 04:46:19 +0200

    feat(app): crud partie 2

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -10,7 +10,7 @@ use App\Form\Admin\UserType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/user')]
+#[Route('/admin/user', name: 'admin_user_')]
 class UserController extends CrudController
 {
     protected string $route = 'admin_user';
@@ -18,25 +18,25 @@ class UserController extends CrudController
     protected string $form = UserType::class;
     protected string $template_path = 'admin/user';
 
-    #[Route('/', methods: ['GET'])]
+    #[Route('', name: 'index', methods: ['GET'])]
     public function index(): Response
     {
         return parent::crudIndex();
     }
 
-    #[Route('/new', methods: ['POST', 'GET'])]
+    #[Route('/new', name: 'new', methods: ['POST', 'GET'])]
     public function new(): Response
     {
         return parent::crudNew();
     }
 
-    #[Route('/{id}/edit', methods: ['POST', 'GET'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['POST', 'GET'])]
     public function edit(User $item): Response
     {
         return parent::crudEdit($item);
     }
 
-    #[Route('/{id}', methods: ['POST', 'DELETE'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST', 'DELETE'])]
     public function delete(User $item): Response
     {
         return parent::crudDelete($item);
